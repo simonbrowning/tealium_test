c1d634f7aa7582cbf3e9a8527b70577f7a8391b3
Simon Browning <simonbrowning86@gmail.com>
2020-12-11 12:22:29 +0000

    Update 0080_Persist_1_Declare_Context_Variables_to_Persist_and_other_general_variables.js
    going old school

--- 0080_Persist_1_Declare_Context_Variables_to_Persist_and_other_general_variables.js
+++ 0080_Persist_1_Declare_Context_Variables_to_Persist_and_other_general_variables.js
@@ -1,4 +1,4 @@
 //this is a quite a long file name
 
-console.log(`something, something, darkside`);
-console.log(`new line`);
+console.log("something, something, darkside");
+console.log("new line");
